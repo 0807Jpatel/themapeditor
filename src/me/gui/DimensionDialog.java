aa90ceb403e12590c002c2d8955095e1744b3c33
0807Jpatel <Jaykumar.patel@stonybrook.edu>
2016-07-02 12:17:21 -0400

    done with the DimensionDialog with css and everything also added event filter to the text fields so it doesn't take anything but numbers

--- src/me/gui/DimensionDialog.java
+++ src/me/gui/DimensionDialog.java
@@ -1,10 +1,14 @@
 package gui;
 
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import properties_manager.PropertiesManager;
 
@@ -13,38 +17,53 @@ import properties_manager.PropertiesManager;
  */
 public class DimensionDialog {
 	private final String CSS_PATH = "css/dialog.css";
+	private final String GRIDPANE_STYLE = "gridpane";
+	private final String BUTTON_WIDTH = "buttonWidth";
+	private final String HBOX = "hBox";
 
-	double height;
-	double widgth;
+	private double height;
+	private double widgth;
 	public void show(){
 		PropertiesManager prop = PropertiesManager.getPropertiesManager();
 		Stage dim = new Stage();
+		dim.setHeight(250);
+		dim.setWidth(350);
+		dim.initModality(Modality.APPLICATION_MODAL);
 		dim.setTitle(prop.getProperty(PropertyType.DIMENSIONS_TITLE_LABEL));
 
 		GridPane gp = new GridPane();
 
 		gp.add(new Label(prop.getProperty(PropertyType.DIMENSION_HEIGHT_LABEL)), 0 , 0);
 		TextField HeightTF = new TextField();
+		HeightTF.addEventFilter(KeyEvent.KEY_TYPED , numeric_Validation());
 		gp.add(HeightTF, 1 , 0);
 
 		gp.add(new Label(prop.getProperty(PropertyType.DIMENSION_WIDTH_LABEL)), 0 , 1);
 		TextField WidthTF = new TextField();
+		WidthTF.addEventFilter(KeyEvent.KEY_TYPED , numeric_Validation());
 		gp.add(WidthTF, 1 , 1);
 
-		Button ok = new Button(prop.getProperty(PropertyType.OK_BUTTON));
-		Button cancel = new Button(prop.getProperty(PropertyType.CANCEL_BUTTON));
+		HBox hbox = new HBox();
+		Button okButton = new Button(prop.getProperty(PropertyType.OK_BUTTON));
+		Button cancelButton = new Button(prop.getProperty(PropertyType.CANCEL_BUTTON));
+		okButton.getStyleClass().add(BUTTON_WIDTH);
+		cancelButton.getStyleClass().add(BUTTON_WIDTH);
+		hbox.getChildren().addAll(cancelButton, okButton);
+		hbox.getStyleClass().add(HBOX);
 
-		gp.add(cancel, 2 , 2);
-		gp.add(ok, 0, 2);
+		gp.add(hbox, 1, 2);
 
+		gp.getStyleClass().add(GRIDPANE_STYLE);
+		okButton.getStyleClass().add(BUTTON_WIDTH);
+		cancelButton.getStyleClass().add(BUTTON_WIDTH);
 
-		ok.setOnAction(e -> {
+		okButton.setOnAction(e -> {
 			height = Double.parseDouble(HeightTF.getText());
 			widgth = Double.parseDouble(WidthTF.getText());
 			dim.close();
 		});
 
-		cancel.setOnAction(e -> {
+		cancelButton.setOnAction(e -> {
 			e.consume();
 			dim.close();
 		});
@@ -54,5 +73,14 @@ public class DimensionDialog {
 		scene.getStylesheets().add(getClass().getClassLoader().getResource(CSS_PATH).toString());
 		dim.setScene(scene);
 		dim.showAndWait();
+	}
+
+
+	private EventHandler<KeyEvent> numeric_Validation() {
+		return e -> {
+			String c = e.getCharacter();
+			if(!"1234567890.".contains(c))
+				e.consume();
+		};
 	}
 }
